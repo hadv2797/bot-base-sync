b3b50c289a24c36dcd0a5654f6d7df14a567e301
hadv2797 <hadv2797@gmail.com>
2026-08-01 14:24:26 +0700

    Update step1_login.py

--- workflow_nghiem_thu/step1_login.py
+++ workflow_nghiem_thu/step1_login.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 
 BASE_URL = "https://workflow.base.vn/qttrienkhaiduandandung-12551"
 USERNAME = "hadv@manfusi.com"
-PASSWORD = "RX8Q4C"
+PASSWORD = "RXZZL48Q4C"
 
 def login_and_get_page(playwright_instance):
     print("--- KHỞI TẠO & ĐĂNG NHẬP BASE ---")
